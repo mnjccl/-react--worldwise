a914939b728c8d5c2cba5b6729aa85659bb9084f
Minja Ćućula <cuculaminja@proton.me>
2024-07-08 18:16:55 +0200

    Initial commit

---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,20 @@
+import styles from "./CountryItem.module.css";
+import PropTypes from "prop-types";
+
+CountryItem.propTypes = {
+  country: PropTypes.shape({
+    emoji: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+function CountryItem({ country }) {
+  return (
+    <li className={styles.countryItem}>
+      <span>{country.emoji}</span>
+      <span>{country.country}</span>
+    </li>
+  );
+}
+
+export default CountryItem;
