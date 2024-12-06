a49ee756ccbe0b839926e499c3318c4c733cd92d
Minja Ćućula <cuculaminja@proton.me>
2024-12-06 08:01:45 +0100

    Adding register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import PageNav from "../components/PageNav";
 import styles from "../css/Register.module.css";
 
 function Register() {
+  const [fullName, setFullName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPassword, setConfirmPassword] = useState<string>();
+
+  const handleSubmit = () => {};
+
   return (
     <main className={styles.register}>
       <PageNav />
-      <section></section>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.row}>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            id="fullName"
+          />
+        </div>
+        <div className={styles.row}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+          />
+        </div>
+        <div className={styles.row}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles.row}>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            id="confirmPassword"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <Button type="primary">Register</Button>
+        </div>
+      </form>
     </main>
   );
 }
